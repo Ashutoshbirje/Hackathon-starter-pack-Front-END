31c7a5962b3afd8965938bad4d3038ce609ecbab
Ashutoshbirje <ashutoshbirje4@gmail.com>
2024-08-19 20:12:27 +0530

    ok

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return(
+    <div className="App">
+       <h1>
+        Create Your Project Here ... !
+       </h1>
+    </div>
+  );
+}
+
+export default App;
